bba3ffe0be8dcc8770f246c0411c39e2148d603f
RaduPopescu95 <73418692+RaduPopescu95@users.noreply.github.com>
2024-11-07 13:17:56 +0000

    tagmanger

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -9,6 +9,7 @@ import ToasterContext from "../context/ToastContext";
 import ScrollToTop from "@/components/Common/ScrollToTop";
 import PreLoader from "@/components/Common/PreLoader";
 import { StripeSessionProvider } from "../context/StripeSessionContext";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
@@ -23,6 +24,19 @@ export default function RootLayout({
 
   return (
     <html lang="ro" suppressHydrationWarning>
+           <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-5ELN600F36"
+      ></Script>
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-5ELN600F36');
+        `}
+      </Script>
       <meta name="google-site-verification" content="4Oy9OuF_U8U8klw8BH9CPtlhGgM23UdFMlAYMGxdE_E" />
       <body>
         {loading ? (
